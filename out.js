2415819270a87eb2564a2a6c1adeea9f60b0eccb
acou12 <51276518+acou12@users.noreply.github.com>
2023-03-14 16:58:09 -0700

    more testing

--- out.js
+++ out.js
@@ -1,5 +1,6 @@
 
 const println = console.log;
 let fact=(x)=>{return (((x <= 1)) ? (1) : ((x * fact((x - 1)))))}
-let main=()=>{return println((fact(150) - fact(150)))}
+let forloop=(from,to,f)=>{return (((from <= to)) ? (()=>{f(from);return forloop((from + 1),to,f)}) : (()=>{}))()}
+let main=()=>{return forloop(0,50,(x)=>{return println(((x + "! = ") + fact(x)))})}
 main()
